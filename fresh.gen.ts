78d33aa26c9f1b4ec4a0e9e75af28ff93854582a
Sean Aye <hello@seanaye.ca>
2023-09-30 19:58:45 -0400

    manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,10 +4,8 @@
 
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/greet/[name].tsx";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/resume.tsx";
+import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/resume.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/GameOfLifeCanvas.tsx";
 
@@ -15,10 +13,8 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/greet/[name].tsx": $3,
-    "./routes/index.tsx": $4,
-    "./routes/resume.tsx": $5,
+    "./routes/index.tsx": $2,
+    "./routes/resume.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
